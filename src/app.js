b2667b8efcccca5717215ed797794b7f3ed5b0f1
Lesley Burdock <lesleyburdock@lesleys-air.speedport.ip>
2024-04-25 13:31:09 +0200

    added javascript for nav responsive

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
-const openCall =()=>{
-    let hamburger = document.getElementById("hamburger");
-    let nav_items = document.getElementById("nav_items");
+
+
+ const openCall =()=>{
+    let nav_items = document.getElementById("nav-menu");
     if (nav_items.style.display === "block") {
         nav_items.style.display = "none";
-      } else {  
+       } else {  
         nav_items.style.display = "block";
-      }
+       }
 }
 
